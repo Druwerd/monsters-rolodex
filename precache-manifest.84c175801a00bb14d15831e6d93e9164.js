5e354acfa2a473a28919c48fffb64bcf10e09b6d
Dru Ibarra <dru.ibarra@fundingcircle.com>
2020-07-14 15:54:45 -0700

    Updates

---
+++ precache-manifest.84c175801a00bb14d15831e6d93e9164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af373b973c0c8cdf14ffafe32550c1c9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a762695545ad56008640",
+    "url": "/monsters-rolodex/static/css/main.35d927dd.chunk.css"
+  },
+  {
+    "revision": "639c49fbd6946a082aa3",
+    "url": "/monsters-rolodex/static/js/2.eb0c56c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.eb0c56c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a762695545ad56008640",
+    "url": "/monsters-rolodex/static/js/main.2c86f530.chunk.js"
+  },
+  {
+    "revision": "c61c615c4d64e0f8bd9e",
+    "url": "/monsters-rolodex/static/js/runtime-main.95c043bc.js"
+  }
+]);
